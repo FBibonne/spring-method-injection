d16b3958065f16b0d7086c4e5b1b5f1342a3b36b
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2024-04-10 11:51:13 +0200

    feat (better handle bad requests)

--- src/main/java/pocaop/otherendpoints/ExceptionsHandler.java
+++ src/main/java/pocaop/otherendpoints/ExceptionsHandler.java
@@ -1,17 +1,21 @@
 package pocaop.otherendpoints;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import pocaop.ControllersConfigurationListener;
 import pocaop.internal.exceptions.ArgumentException;
 
-@ControllerAdvice("${"+ ControllersConfigurationListener.INTERFACE_CONTROLLERS_PACKAGE_KEY +"}")
+@ControllerAdvice()
 public record ExceptionsHandler() {
 
-    @ExceptionHandler({UnsupportedOperationException.class, ArgumentException.class})
-    public ResponseEntity<String> handleUnsupportedOperationException(UnsupportedOperationException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    @ExceptionHandler({UnsupportedOperationException.class, ArgumentException.class, HttpMessageNotWritableException.class})
+    // cf. excellent article about Spring MVC errors handling : https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc
+    public ResponseEntity<String> handleException(Exception exception) {
+        return switch (exception){
+            case HttpMessageNotWritableException _ ->  ResponseEntity.badRequest().body("Specify a valid value for Accept and Accept-Content : application/json or application/xml");
+            default -> ResponseEntity.badRequest().body(exception.getMessage());
+        };
     }
 
 }
