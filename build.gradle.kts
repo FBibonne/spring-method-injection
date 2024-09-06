72e25927b32db6a35b56d8da7640c0724cff23be
Fabrice Bibonne <fabrice.bibonne@gmail.com>
2024-09-06 19:34:57 +0200

    fix (gradle conf for github package) : - use get() over a property provider - set username and token properties in $GRADLE_USER_HOME/gradle.properties without quote

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,30 +48,35 @@ repositories {
     maven{
         url = uri("https://maven.pkg.github.com/FBibonne/Properties-Logger")
         credentials{
-            // set the property github.username in $GRADLE_HOME/gradle.properties file like this `github.username='FBibonne'`
-            //username = providers.gradleProperty("github.username").toString()
-            username = System.getenv("GITHUB_USERNAME")
-            // set the property github.user_token in $GRADLE_HOME/gradle.properties file like this `github.user_token='ghp_****'`
-            //password = providers.gradleProperty("github.user_token").toString()
-            password = System.getenv("GITHUB_TOKEN_PACKAGE")
+            // set the property github.username in $GRADLE_HOME/gradle.properties file like this `github.username=FBibonne`
+            username = providers.gradleProperty("github.username").get()
+            // set the property github.user_token in $GRADLE_HOME/gradle.properties file like this `github.user_token=ghp_****`
+            password = providers.gradleProperty("github.user_token").get()
         }
     }
     mavenLocal()
 }
 
+// VERSION PROPERTIES
+val propertiesLoggerVersion = "1.0.1"
+
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    annotationProcessor("org.projectlombok:lombok")
+
     compileOnly("org.projectlombok:lombok")
     compileOnly("org.springframework:spring-core")
     compileOnly("org.springframework:spring-web")
     compileOnly("org.springframework:spring-aop")
     compileOnly("org.springframework:spring-context")
     compileOnly("org.springframework.boot:spring-boot-starter-logging")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+
     testImplementation("org.springframework.boot:spring-boot-starter-security")
-    testImplementation("fr.insee:boot-properties-logger-starter:1.0.0-SNAPSHOT")
-    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("fr.insee:boot-properties-logger-starter:$propertiesLoggerVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
